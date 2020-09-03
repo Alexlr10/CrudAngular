9fb09a62fddee69e646af912822f069961a2172f
Alex Lopes <alexlr10@hotmail.com>
2020-09-03 19:48:34 -0300

    implementando metodo post

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,6 +10,10 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+    product: Product = {
+        name: '',
+        price: null
+    }
     constructor(private productService: ProductService,
     private router: Router) { }
 
@@ -17,7 +22,10 @@ export class ProductCreateComponent implements OnInit {
     }
 
     createProduct(): void {
-        this.productService.showMessage('Produto Criado')
+        this.productService.create(this.product).subscribe(() => {
+            this.productService.showMessage('Produto Criado')
+            this.router.navigate(['products'])
+        })
     }
 
     cancel(): void {
